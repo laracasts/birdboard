a0b8af6de84936b627f229059202ac02ab09c48b
Jeffrey Way <jeffrey@jeffrey-way.com>
2019-05-03 10:36:54 -0400

    Fix more redirect paths

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/projects';
 
     /**
      * Create a new controller instance.
