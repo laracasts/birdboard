4d5e7f34c19a5bd401b9cad776b657b217ffd8cd
Jeffrey Way <jeffrey@jeffrey-way.com>
2019-05-03 10:31:49 -0400

    Redirect to projects dashboard on login

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/projects';
 
     /**
      * Create a new controller instance.
